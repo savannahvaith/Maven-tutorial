c9a1508f701c8366bbb316a67cc97636bb97bef5
Savannah Vaith <sav.vaithilingam@gmail.com>
2020-04-20 14:20:17 +0100

    Maven tutorial; jar with dependencies

---
+++ src/main/java/com/qa/maventutorial/Runner.java
@@ -0,0 +1,15 @@
+package com.qa.maventutorial;
+
+import org.apache.commons.lang3.RandomUtils;
+
+public class Runner {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(RandomUtils.nextInt());
+//		System.out.println("Hello!");
+		
+		
+	}
+
+}
